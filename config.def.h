111faa7c2b619cac5908e380a78582b9172c0374
Vasiliy Aboymov <aboymov@gmail.com>
2021-12-07 21:33:58 +0300

    Added a rules for Steam

--- config.def.h
+++ config.def.h
@@ -13,8 +13,8 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-//static const char col_cyan[]        = "#005577";
-static const char col_cyan[]        = "#526865";
+static const char col_cyan[]        = "#005577";
+//static const char col_cyan[]        = "#526865";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -32,6 +32,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Steam",    NULL,       NULL,       1 << 1,       0,           -1 },
 };
 
 /* layout(s) */
